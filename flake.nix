82c2e181a103166504eafee2cbe14e9ebdbf8de7
binarylinuxx <aar58384@gmail.com>
2026-07-22 04:22:43 +0300

    added session handling

--- flake.nix
+++ flake.nix
@@ -4,14 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
     halley-stable-src = {
       url = "github:saltnpepper97/halley/v0.5.0";
       flake = false;
     };
+
     halley-unstable-src = {
       url = "github:saltnpepper97/halley/main";
       flake = false;
     };
+
     halley-unstable-dev-src = {
       url = "github:saltnpepper97/halley/dev";
       flake = false;
@@ -19,28 +22,51 @@
   };
 
   outputs =
-    { self, nixpkgs, flake-utils, halley-stable-src, halley-unstable-src, halley-unstable-dev-src }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      halley-stable-src,
+      halley-unstable-src,
+      halley-unstable-dev-src,
+    }:
     let
-      nixosModule = { pkgs, lib, config, ... }: let
-        halleyPkgs = self.packages.${pkgs.system};
-      in {
-        options.programs.halley = {
-          enable = lib.mkEnableOption "Halley Wayland compositor";
-          package = lib.mkOption {
-            type = lib.types.package;
-            default = halleyPkgs.halley-unstable;
-            description = "Halley package to use";
+      nixosModule =
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        let
+          halleyPkgs = self.packages.${pkgs.system};
+        in
+        {
+          options.programs.halley = {
+            enable = lib.mkEnableOption "Halley Wayland compositor";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = halleyPkgs.halley-unstable;
+              description = "Halley package to use";
+            };
+          };
+
+          config = lib.mkIf config.programs.halley.enable {
+            environment.systemPackages = [
+              config.programs.halley.package
+            ];
+
+            services.displayManager.sessionPackages = [
+              config.programs.halley.package
+            ];
+
+            environment.etc."xdg/xdg-desktop-portal/halley-portals.conf".source =
+              "${config.programs.halley.package}/share/xdg-desktop-portal/halley-portals.conf";
           };
         };
-        config = lib.mkIf config.programs.halley.enable {
-          environment.systemPackages = [ config.programs.halley.package ];
-
-          # Portal config: prefer Halley for ScreenCast/Screenshot, GTK for the rest
-          environment.etc."xdg/xdg-desktop-portal/halley-portals.conf".source =
-            "${config.programs.halley.package}/share/xdg-desktop-portal/halley-portals.conf";
-        };
-      };
-    in flake-utils.lib.eachDefaultSystem (
+    in
+    flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -64,125 +90,151 @@
           wayland
           wayland-protocols
           clang
+          makeWrapper
         ];
 
         mkHalley =
-          { pname, version, src, cargoLock
-          , extraBuildInputs ? [ ]
-          , extraNativeBuildInputs ? [ ]
-          , runtimeLibs ? [ ]
-          , meta
-          , doCheck ? true
+          {
+            pname,
+            version,
+            src,
+            cargoLock,
+            extraBuildInputs ? [ ],
+            runtimeLibs,
+            meta,
+            doCheck ? false,
           }:
           pkgs.rustPlatform.buildRustPackage {
-            inherit pname version src cargoLock meta doCheck;
+            inherit
+              pname
+              version
+              src
+              cargoLock
+              meta
+              doCheck
+              ;
 
-            nativeBuildInputs = commonNativeBuildInputs ++ extraNativeBuildInputs
-              ++ [ pkgs.makeWrapper ];
-
+            nativeBuildInputs = commonNativeBuildInputs;
             buildInputs = commonBuildInputs ++ extraBuildInputs;
 
             LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
-            postInstall = let
-              runtimePath = pkgs.lib.makeLibraryPath runtimeLibs;
-            in ''
-              wrapProgram $out/bin/halley \
-                --prefix LD_LIBRARY_PATH : ${runtimePath}
-              wrapProgram $out/bin/halleyctl \
-                --prefix LD_LIBRARY_PATH : ${runtimePath}
+            postInstall =
+              let
+                runtimePath = pkgs.lib.makeLibraryPath runtimeLibs;
+              in
+              ''
+                wrapProgram "$out/bin/halley" \
+                  --prefix LD_LIBRARY_PATH : "${runtimePath}"
 
-              if [ -f "$out/bin/xdg-desktop-portal-halley" ]; then
-                wrapProgram $out/bin/xdg-desktop-portal-halley \
-                  --prefix LD_LIBRARY_PATH : ${runtimePath}
-              fi
+                wrapProgram "$out/bin/halleyctl" \
+                  --prefix LD_LIBRARY_PATH : "${runtimePath}"
 
-              # Session files
-              install -Dm755 $src/packaging/wayland-sessions/halley-session -t $out/bin
-              install -Dm644 $src/packaging/wayland-sessions/halley.desktop \
-                $out/share/wayland-sessions/halley.desktop
+                wrapProgram "$out/bin/xdg-desktop-portal-halley" \
+                  --prefix LD_LIBRARY_PATH : "${runtimePath}"
 
-              # Patch hardcoded /usr/bin paths in session launcher
-              substituteInPlace $out/bin/halley-session \
-                --replace-fail '/usr/bin/halley' "$out/bin/halley"
+                install -Dm755 \
+                  "$src/packaging/wayland-sessions/halley-session" \
+                  "$out/bin/halley-session"
 
+                install -Dm644 \
+                  "$src/packaging/wayland-sessions/halley.desktop" \
+                  "$out/share/wayland-sessions/halley.desktop"
 
-              # Portal backend config
-              if [ -d "$src/packaging/xdg-desktop-portal" ]; then
-                mkdir -p $out/share/xdg-desktop-portal/portals
-                cp -t $out/share/xdg-desktop-portal/portals \
-                  $src/packaging/xdg-desktop-portal/portals/halley.portal 2>/dev/null || true
-                cp -t $out/share/xdg-desktop-portal \
-                  $src/packaging/xdg-desktop-portal/halley-portals.conf 2>/dev/null || true
-              fi
+                substituteInPlace "$out/bin/halley-session" \
+                  --replace-fail "/usr/bin/halley" "$out/bin/halley"
 
-              # D-Bus activation for portal
-              if [ -d "$src/packaging/dbus-1/services" ]; then
-                mkdir -p $out/share/dbus-1/services
-                cp -t $out/share/dbus-1/services \
-                  $src/packaging/dbus-1/services/*.service
-                substituteInPlace $out/share/dbus-1/services/*.service \
-                  --replace-fail '/usr/bin/' "$out/bin/"
-              fi
+                substituteInPlace "$out/share/wayland-sessions/halley.desktop" \
+                  --replace-fail "Exec=/usr/bin/halley-session" \
+                    "Exec=$out/bin/halley-session"
 
-              # systemd user units
-              if [ -d "$src/packaging/systemd-user" ]; then
-                mkdir -p $out/lib/systemd/user
-                cp -t $out/lib/systemd/user \
-                  $src/packaging/systemd-user/halley.service \
-                  $src/packaging/systemd-user/halley-shutdown.target
+                install -Dm644 \
+                  "$src/packaging/xdg-desktop-portal/portals/halley.portal" \
+                  "$out/share/xdg-desktop-portal/portals/halley.portal"
 
-                # Patch hardcoded /usr/bin paths in systemd units
-                substituteInPlace $out/lib/systemd/user/halley.service \
-                  --replace-fail '/usr/bin/halley' "$out/bin/halley"
-              fi
-            '';
+                install -Dm644 \
+                  "$src/packaging/xdg-desktop-portal/halley-portals.conf" \
+                  "$out/share/xdg-desktop-portal/halley-portals.conf"
+
+                install -Dm644 \
+                  "$src/packaging/dbus-1/services/"*.service \
+                  -t "$out/share/dbus-1/services"
+
+                substituteInPlace "$out/share/dbus-1/services/"*.service \
+                  --replace-fail "/usr/bin/" "$out/bin/"
+
+                install -Dm644 \
+                  "$src/packaging/systemd-user/halley.service" \
+                  "$out/lib/systemd/user/halley.service"
+
+                install -Dm644 \
+                  "$src/packaging/systemd-user/halley-shutdown.target" \
+                  "$out/lib/systemd/user/halley-shutdown.target"
+
+                substituteInPlace "$out/lib/systemd/user/halley.service" \
+                  --replace-fail "/usr/bin/halley" "$out/bin/halley"
+              '';
           };
       in
       {
         packages = rec {
           halley-stable = mkHalley {
             pname = "halley";
-            version = "0.4.0";
+            version = "0.5.0";
             src = halley-stable-src;
-            cargoLock = {
-              lockFile = halley-stable-src + "/Cargo.lock";
-            };
-            doCheck = false;
-            runtimeLibs = with pkgs; [ libglvnd libgbm mesa ];
+
+            cargoLock.lockFile = halley-stable-src + "/Cargo.lock";
+
+            runtimeLibs = with pkgs; [
+              libglvnd
+              libgbm
+              mesa
+            ];
+
             meta = with pkgs.lib; {
               description =
                 "Spatial Wayland compositor built around infinite workspace navigation";
               homepage = "https://github.com/saltnpepper97/halley";
               license = licenses.gpl3Only;
               platforms = platforms.linux;
-              maintainers = [ ];
               mainProgram = "halley";
             };
           };
 
           halley-unstable = mkHalley {
             pname = "halley";
-            version = "0.5.0";
+            version = "0.5.0-unstable";
             src = halley-unstable-src;
+
             cargoLock = {
               lockFile = halley-unstable-src + "/Cargo.lock";
+
               outputHashes = {
                 "smithay-0.7.0" =
                   "sha256-TV/GTfSvgfVwIFUGoASU7xm38opIBLjLMf1HeNTW07U=";
               };
             };
-            extraBuildInputs = with pkgs; [ pipewire dbus ];
-            doCheck = false;
-            runtimeLibs = with pkgs; [ libglvnd libgbm mesa wayland pipewire dbus ];
-            extraNativeBuildInputs = [ ];
+
+            extraBuildInputs = with pkgs; [
+              pipewire
+              dbus
+            ];
+
+            runtimeLibs = with pkgs; [
+              libglvnd
+              libgbm
+              mesa
+              wayland
+              pipewire
+              dbus
+            ];
+
             meta = with pkgs.lib; {
               description =
-                "Spatial Wayland compositor (unstable/git) built around infinite workspace navigation";
+                "Spatial Wayland compositor from the main branch";
               homepage = "https://github.com/saltnpepper97/halley";
               license = licenses.gpl3Only;
               platforms = platforms.linux;
-              maintainers = [ ];
               mainProgram = "halley";
             };
           };
@@ -191,24 +243,36 @@
             pname = "halley";
             version = "0.5.0-dev";
             src = halley-unstable-dev-src;
+
             cargoLock = {
               lockFile = halley-unstable-dev-src + "/Cargo.lock";
+
               outputHashes = {
                 "smithay-0.7.0" =
                   "sha256-TV/GTfSvgfVwIFUGoASU7xm38opIBLjLMf1HeNTW07U=";
               };
             };
-            extraBuildInputs = with pkgs; [ pipewire dbus ];
-            doCheck = false;
-            runtimeLibs = with pkgs; [ libglvnd libgbm mesa wayland pipewire dbus ];
-            extraNativeBuildInputs = [ ];
+
+            extraBuildInputs = with pkgs; [
+              pipewire
+              dbus
+            ];
+
+            runtimeLibs = with pkgs; [
+              libglvnd
+              libgbm
+              mesa
+              wayland
+              pipewire
+              dbus
+            ];
+
             meta = with pkgs.lib; {
               description =
-                "Spatial Wayland compositor (dev branch) built around infinite workspace navigation";
+                "Spatial Wayland compositor from the development branch";
               homepage = "https://github.com/saltnpepper97/halley";
               license = licenses.gpl3Only;
               platforms = platforms.linux;
-              maintainers = [ ];
               mainProgram = "halley";
             };
           };
@@ -216,15 +280,16 @@
           default = halley-unstable;
         };
 
-        apps = {
-          default = flake-utils.lib.mkApp {
-            drv = self.packages.${system}.default;
-            name = "halley";
-          };
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+          name = "halley";
         };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.halley-unstable ];
+          inputsFrom = [
+            self.packages.${system}.halley-unstable
+          ];
+
           nativeBuildInputs = with pkgs; [
             cargo
             rustc
@@ -232,12 +297,12 @@
             clippy
             rustfmt
           ];
-          shellHook = ''
-            export RUST_LOG=info
-          '';
+
+          RUST_LOG = "info";
         };
       }
-    ) // {
+    )
+    // {
       nixosModules.default = nixosModule;
     };
 }
